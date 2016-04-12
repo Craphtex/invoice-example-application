a1819b2524da05200e02a1268feea5d398d74e53
Christian Lindgren <christian@crapht.se>
2016-04-12 15:34:45 +0900

    Limit unspecified amount of return element during subscription to the default amount

--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,4 @@
 Meteor.publish('invoices', function(date, days, limit) {
-  limit = limit || 10;
-  if (limit > 0) {
-    return InvoiceTicketsCollection.find(getFilter(date, days), {limit: limit});
-  }
-  else {
-  	return InvoiceTicketsCollection.find(getFilter(date, days));
-  }
+  if ((limit || 0) < 1) limit = 10;
+  return InvoiceTicketsCollection.find(getFilter(date, days), {limit: limit});
 });
