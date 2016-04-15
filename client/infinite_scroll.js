baf13f8310ed9112e85b8a793ddebbe82ec69dd9
Christian Lindgren <christian@crapht.se>
2016-04-15 18:54:28 +0900

    Added default debounce value

--- client/infinite_scroll.js
+++ client/infinite_scroll.js
@@ -1,6 +1,9 @@
+const DEFAULT_DEBOUNCE = 200;
+
 Template.InfiniteScroll.onCreated(() => {
   showMoreVisible = (eventName) => {
     let target = this.$('.infiniteScroller');
+    console.log(Template.currentData());
     if (!target.length) return;
 
     let threshold = $(window).scrollTop() + $(window).height() - target.height();
@@ -12,9 +15,9 @@ Template.InfiniteScroll.onCreated(() => {
 });
 
 Template.InfiniteScroll.onRendered(function() {
-  $(window).on('scroll', _.debounce(() => {showMoreVisible(this.data.eventName)}, 200));
+  $(window).on('scroll', _.debounce(() => {showMoreVisible(this.data.eventName)}, Template.currentData().debounce || DEFAULT_DEBOUNCE));
 });
 
 Template.InfiniteScroll.onDestroyed(function() {
-  $(window).off('scroll', _.debounce(() => {showMoreVisible(this.data.eventName)}, 200));
+  $(window).off('scroll', _.debounce(() => {showMoreVisible(this.data.eventName)}, Template.currentData().debounce || DEFAULT_DEBOUNCE));
 });
