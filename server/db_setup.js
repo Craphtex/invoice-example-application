1415a518ddf7e925e94684d9f09d9f24507cf3d0
Christian Lindgren <christian@crapht.se>
2016-04-20 15:25:17 +0900

    #22 Add email field to the database

--- server/db_setup.js
+++ server/db_setup.js
@@ -13,7 +13,10 @@ createTickets = function(startDate, days, numberOfTickets) {
     createdAt.setSeconds(0);
     createdAt.setMilliseconds(0);
     let total = Math.floor(Math.random() * 20) * 10;
-    InvoiceTicketsCollection.insert({"invoiceNumber": invoiceNumber, "total": total, "createdAt": createdAt});
+    let names = ['smith', 'jones', 'taylor', 'brown', 'williams', 'wilson', 'johnson', 'davies', 'robinson', 'wright', 'thompson', 'evans', 'walker', 'white', 'roberts', 'green', 'hall', 'wood', 'jackson', 'clarke'];
+    let domains = ['gmail.com', 'hotmail.com', 'aol.com', 'yahoo.com', 'me.com', 'iohk.io'];
+    let email = names[Math.floor(Math.random()*names.length)] + '+' + Math.floor(Math.random() * 10) + '@' + domains[Math.floor(Math.random()*domains.length)];
+    InvoiceTicketsCollection.insert({'invoiceNumber': invoiceNumber, 'email': email, 'total': total, 'createdAt': createdAt});
   }
 }
 createTicketsForFirstWeek = function() {
