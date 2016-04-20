b98ff33b7dc0fe189f4c08b1f1eb3d53fa9ee82f
Christian Lindgren <christian@crapht.se>
2016-04-20 13:51:54 +0900

    Moved away function from the global scope

--- client/infinite_scroll.js
+++ client/infinite_scroll.js
@@ -2,24 +2,24 @@ const DEFAULT_DEBOUNCE = 200;
 
 TemplateController('infinite_scroll', {
   onCreated() {
-    showMoreVisible = (eventName) => {
+    this.checkIndicatorVisibility = () => {
       let target = this.$('.infiniteScroller');
       if (!target.length) return;
 
       let threshold = $(window).scrollTop() + $(window).height() - target.height();
 
       if (target.offset().top < threshold) {
-        target.trigger(eventName);
+        target.trigger(this.data.eventName);
         //this.triggerEvent(eventName); // Won't work due to a bug in TemplateController package
       }
     }
   },
 
   onRendered() {
-    $(window).on('scroll', _.debounce(() => {showMoreVisible(this.data.eventName)}, Template.currentData().debounce || DEFAULT_DEBOUNCE));
+    $(window).on('scroll', _.debounce(this.checkIndicatorVisibility, Template.currentData().debounce || DEFAULT_DEBOUNCE));
   },
 
   onDestroyed() {
-    $(window).off('scroll', _.debounce(() => {showMoreVisible(this.data.eventName)}, Template.currentData().debounce || DEFAULT_DEBOUNCE));
+    $(window).off('scroll', _.debounce(this.checkIndicatorVisibility, Template.currentData().debounce || DEFAULT_DEBOUNCE));
   }
 });
